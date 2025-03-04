38a4ace57a5aa2e912318641f26cbba537d2d16a
Cestlaviezzy <m15250145089@163.com>
2025-03-04 09:24:12 +0800

    push commont

--- main.go
+++ main.go
@@ -2,50 +2,8 @@ package main
 
 import (
 	"fmt"
-	"regexp"
-	"strings"
+	"web3-wallet/common"
 )
-
-// 判断地址类型
-func getChainByAddress(address string) string {
-	address = strings.TrimSpace(address)
-
-	if len(address) == 0 {
-		return "Invalid address"
-	}
-
-	if strings.HasPrefix(address, "1") || strings.HasPrefix(address, "3") || strings.HasPrefix(address, "bc1") {
-		return "BTC"
-	}
-
-	if strings.HasPrefix(address, "0x") && len(address) == 42 {
-		return "ETH/BSC"
-	}
-
-	if len(address) == 44 && isBase58(address) {
-		return "SOL"
-	}
-
-	if strings.HasPrefix(address, "T") && len(address) == 34 {
-		return "TRX"
-	}
-
-	if strings.HasPrefix(address, "f1") || strings.HasPrefix(address, "f3") || strings.HasPrefix(address, "f5") {
-		return "FIL"
-	}
-	return "Unknown address"
-}
-
-func isBase58(s string) bool {
-	regex := "^[1-9A-HJ-NP-Za-km-z]+$"
-	//re := regexp.MustCompile(regex)
-	re, err := regexp.Compile(regex)
-	if err != nil {
-		fmt.Println("Invalid base58", err)
-		return false
-	}
-	return re.MatchString(s)
-}
 
 func main() {
 	addresses := []string{
@@ -58,7 +16,7 @@ func main() {
 	}
 
 	for _, address := range addresses {
-		chain := getChainByAddress(address)
+		chain := common.GetChainByAddress(address)
 		fmt.Println(address, chain)
 	}
 }
